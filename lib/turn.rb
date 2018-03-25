eb6fe587fcadc066c3c77d08c50bca1a34422ca5
Robert D Gauthier <rgauthie@gmail.com>
2018-03-25 20:58:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ end
 def valid_move? (board, index)
   if (!index.between?(0, 8))
     return false
-  elsif (position_taken? (board, index))
+  elsif (position_taken?(board, index))
     return false
   else
     return true
